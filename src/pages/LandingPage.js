b3dec5f2666f0e4fc3cb0a8313f2b21aa3eee23e
vegardloewe <vegardjl@gmail.com>
2023-02-17 21:51:25 +0100

    commit

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../styling/style.css'
-import Nav from '../components/1 - Nav'
-import Header from '../components/2 - Header'
-import Partners from '../components/3 - Partners'
-import Features from '../components/4 - Features'
-import Mobile from '../components/5 - Mobile'
-import How from '../components/6 - How'
-import Clients from '../components/7 - Clients'
-import Cta from '../components/8 - Cta'
-import Footer from '../components/9 - Footer'
+import Nav from '../components/1_Nav'
+import Header from '../components/2_Header'
+import Partners from '../components/3_Partners'
+import Features from '../components/4_Features'
+import Mobile from '../components/5_Mobile'
+import How from '../components/6_How'
+import Clients from '../components/7_Clients'
+import Cta from '../components/8_Cta'
+import Footer from '../components/9_Footer'
 
 const LandingPage = () => {
   return (
